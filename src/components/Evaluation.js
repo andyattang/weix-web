6efa02fd0a26bf1fb0fa1a377c0329eaea4fdbf1
Andy Tang <andyattang@sina.com>
2018-03-21 22:30:21 +0800

    added images

--- src/components/Evaluation.js
+++ src/components/Evaluation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
 import { Button, Progress, TabBar } from 'antd-mobile';
-import {LeftBubble, RightBubble} from './Bubble';
+import { LeftBubble, RightBubble } from './Bubble';
 import styles from './Evaluation.css';
 
 const questions = [
@@ -11,50 +11,50 @@ const questions = [
 
 const scrollToBottom = () => {
   let box = $('.am-tabs-pane-wrap-active');
-  box.animate({scrollTop: box.get(0).scrollHeight}, 1000);
-  $('#maskLayer').height($('#answeredDiv').height()+10);
+  box.animate({ scrollTop: box.get(0).scrollHeight }, 1000);
+  $('#maskLayer').height($('#answeredDiv').height() + 10);
 };
 
 const StartEvaluation = () => {
   return (
     <div id="evalBox" className={styles.main}>
       <div className={styles.main} onClick={(e) => {
-         e.preventDefault();
-         scrollToBottom();
-       }}>
-        <Progress percent={30} position="fixed"/>
+        e.preventDefault();
+        scrollToBottom();
+      }}>
+        <Progress percent={30} position="fixed" />
         <div className={styles.eval}>
           <div id="maskLayer" className={styles.maskLayer}></div>
           <div id="answeredDiv" className={styles.answeredBubbleDiv}>
-            <LeftBubble message={questions[0]}/>
-            <RightBubble message="有差异"/>
-            <LeftBubble message={questions[1]}/>
-            <RightBubble message="完全一致"/>
-            <LeftBubble message={questions[0]}/>
-            <RightBubble message="有差异"/>
-            <LeftBubble message={questions[1]}/>
-            <RightBubble message="完全一致"/>
-            <LeftBubble message={questions[0]}/>
-            <RightBubble message="有差异"/>
-            <LeftBubble message={questions[1]}/>
-            <RightBubble message="完全一致"/>
-            <LeftBubble message={questions[0]}/>
-            <RightBubble message="有差异"/>
+            <LeftBubble message={questions[0]} />
+            <RightBubble message="有差异" />
+            <LeftBubble message={questions[1]} />
+            <RightBubble message="完全一致" />
+            <LeftBubble message={questions[0]} />
+            <RightBubble message="有差异" />
+            <LeftBubble message={questions[1]} />
+            <RightBubble message="完全一致" />
+            <LeftBubble message={questions[0]} />
+            <RightBubble message="有差异" />
+            <LeftBubble message={questions[1]} />
+            <RightBubble message="完全一致" />
+            <LeftBubble message={questions[0]} />
+            <RightBubble message="有差异" />
           </div>
           <div className={styles.notAnsweredBubbleDiv}>
-            <LeftBubble message={questions[1]}/>
+            <LeftBubble message={questions[1]} />
           </div>
         </div>
       </div>
       <div className={styles.buttons}>
         <div className={styles.leftButton}><a onClick={(e) => {
-         e.preventDefault();
-         scrollToBottom();
-       }} className={styles.button}>有差异</a></div>
+          e.preventDefault();
+          scrollToBottom();
+        }} className={styles.button}>有差异</a></div>
         <div className={styles.rightButton}><a onClick={(e) => {
-         e.preventDefault();
-         scrollToBottom();
-       }} className={styles.button}>完全一致</a></div>
+          e.preventDefault();
+          scrollToBottom();
+        }} className={styles.button}>完全一致</a></div>
       </div>
     </div>
   );
--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -11,32 +11,32 @@ class MainLayout extends React.Component {
     this.state = {
       selectedTab: 'evlTab',
       hidden: false,
-      location: props.location?props.location:'idx',
+      location: props.location ? props.location : 'idx',
     };
   }
 
   renderContent(pageText) {
     return (
-      <div style={{ position: 'absolute',top:0, backgroundColor: 'white', width: '100%',height: '100%', textAlign: 'center' }}>
+      <div style={{ position: 'absolute', top: 0, backgroundColor: 'white', width: '100%', height: '100%', textAlign: 'center' }}>
         <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
         <a style={{ display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9' }}
-           onClick={(e) => {
+          onClick={(e) => {
             e.preventDefault();
             this.setState({
               hidden: !this.state.hidden,
             });
           }}
-          >
+        >
           Click to show/hide tab-bar
         </a>
         <a style={{ display: 'block', marginBottom: 600, color: '#108ee9' }}
-           onClick={(e) => {
+          onClick={(e) => {
             e.preventDefault();
             this.setState({
               fullScreen: !this.state.fullScreen,
             });
           }}
-          >
+        >
           Click to switch fullscreen
         </a>
       </div>
@@ -44,35 +44,37 @@ class MainLayout extends React.Component {
   }
 
   renderEval() {
-    return this.state.location === 'idx'?(
+    return this.state.location === 'idx' ? (
       <StartEvaluation></StartEvaluation>
-    ):(
-      <Evaluation></Evaluation>
-    );
+    ) : (
+        <Evaluation></Evaluation>
+      );
   }
 
   render() {
     return (
-      <div style={{position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+      <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0 }}>
         <TabBar
           unselectedTintColor="#949494"
           tintColor="#33A3F4"
           barTintColor="white"
           hidden={this.state.hidden}
-          >
+        >
           <TabBar.Item
             icon={
               <div style={{
                 width: '22px',
                 height: '22px',
-                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat'
+              }}
               />
             }
             selectedIcon={
               <div style={{
                 width: '22px',
                 height: '22px',
-                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat'
+              }}
               />
             }
             title="零度测试"
@@ -84,7 +86,7 @@ class MainLayout extends React.Component {
               });
             }}
             data-seed="logId1"
-            >
+          >
             {this.renderEval()}
           </TabBar.Item>
           <TabBar.Item
@@ -92,14 +94,16 @@ class MainLayout extends React.Component {
               <div style={{
                 width: '22px',
                 height: '22px',
-                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat' }}
+                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat'
+              }}
               />
             }
             selectedIcon={
               <div style={{
                 width: '22px',
                 height: '22px',
-                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat' }}
+                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat'
+              }}
               />
             }
             title="关系测试"
@@ -111,7 +115,7 @@ class MainLayout extends React.Component {
                 selectedTab: 'friendTab',
               });
             }}
-            >
+          >
             {this.renderContent('关系测试')}
           </TabBar.Item>
           <TabBar.Item
@@ -125,7 +129,7 @@ class MainLayout extends React.Component {
                 selectedTab: 'myTab',
               });
             }}
-            >
+          >
             {this.renderContent('个人中心')}
           </TabBar.Item>
         </TabBar>
